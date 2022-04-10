ed3bed247116afc09ccf92d8d8ea061ac25c255a
DzDeViL <103369592+DzDeViL@users.noreply.github.com>
2022-04-10 14:42:53 +0100

    Create XssDevil.py

---
+++ XssDevil.py
@@ -0,0 +1,42 @@
+import requests
+import threading
+from colorama import Fore, Back, Style
+
+
+print(
+        Fore.YELLOW +
+         """
+________                                             
+\______ \  __ __  _____ _________  ___  ______ ______
+ |    |  \|  |  \/     \\____ \  \/  / /  ___//  ___/
+ |    `   \  |  /  Y Y  \  |_> >    <  \___ \ \___ \ 
+/_______  /____/|__|_|  /   __/__/\_ \/____  >____  >
+        \/            \/|__|        \/     \/     \/ 
+                                          [ twitter.com/r00t_nasser ]
+                                          [ Snapchat : aaa.saq ]
+        """ + Fore.RESET)
+
+print()
+print()
+url = input(input("Please Enter Target Url\t:"))
+payloads = open('payloads.txt','r')
+def Send_req(url,payload):
+    #while url[-1] != '=':
+     #   url = url[:-1]
+    url = url.replace("=",f"={payload}")
+
+    try:
+
+        res = requests.get(url)
+        if payload in res.text:
+           print(Fore.GREEN +'XSS Found   -->','   ' , f"{url}" + Fore.RESET)
+
+
+    except Exception as e:
+        pass
+file = file.readlines()
+for payload in payloads:
+    for url in file:
+        url = url.strip('\n')
+        payload = payload.strip('\n')
+        threading.Thread(target=Send_req,args=(url,payload,)).start()
